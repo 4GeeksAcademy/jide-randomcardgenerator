6331d8bae8d03706b1f9a3ed5080c4df76ce2606
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-05-29 19:21:00 +0000

    card changes with js array values

--- src/app.js
+++ src/app.js
@@ -3,9 +3,37 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = () => {
-  document.querySelector(".card").classList.add("heart");
-  setTimeout(() => {
-    document.querySelector(".card").classList.remove("heart");
-    document.querySelector(".card").classList.add("spade");
-  }, 1000); // 1000 millisecods = 1 second
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+/* setTimeout(() => {
+  document.querySelector(".card").classList.remove("heart");
+  document.querySelector(".card").classList.add("spade");
+}, 1000); // 1000 milliseconds = 1 second */
